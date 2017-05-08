27e9d826349fa05c9aebc291eba455e0cae8ad09
KhasMek <Boushh@gmail.com>
2017-05-07 20:52:28 -0600

    nessus: unify console output
      To do so, we'll stop using scan_run() and call the
      action manually and replicate our own scanstatus
      function heavily based off off libnessus'
      private _scan_status() function.

--- core/modules/nessus.py
+++ core/modules/nessus.py
@@ -5,10 +5,16 @@ Woo nessus scanning
 
 import logging
 import os
+import requests
+import sys
+import time
 
+from core.helpers.misc import suppress_stdout
+from core.helpers.term import ctact, ctinfo
 from core.parsers.config import ParseConfig
 from itertools import zip_longest
 from nessrest import ness6rest
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 
 class Nessus:
@@ -25,6 +31,7 @@ class Nessus:
         self.insecure = parseconfig.nessus_insecure
 
     def main(self):
+        print("{a} NESSUS".format(a=ctact))
         logging.debug("STARTING - nessus")
         logging.debug("Targets - {t}".format(t=', '.join(self.targets)))
         args = [iter(self.targets)] * 10
@@ -46,20 +53,51 @@ class Nessus:
                            .join([scan_name.replace(" ", "-"), "nessus"])])
             logging.debug("Filename - {f}".format(f=xml))
             self.writeoutput(xml, results)
-        print("Finished running Nessus on {t}".format(t=self.targets))
+        print("{a} NESSUS COMPLETE".format(a=ctact))
         logging.debug("COMPLETED - nessus")
 
-    @staticmethod
-    def runnessus(url, api_akey, api_skey, policy, targets, scan_name, insecure):
+    def runnessus(self, url, api_akey, api_skey, policy, targets, scan_name, insecure):
+        if insecure:
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
         scan = ness6rest.Scanner(url=url, api_akey=api_akey, api_skey=api_skey,
                                  insecure=insecure)
         scan.policy_set(name=policy)
         logging.debug("TARGETS: {t}".format(t=targets))
         scan.scan_add(targets=targets, name=scan_name)
-        scan.scan_run()
-        scan.scan_results()
-        output = str(scan.download_scan(export_format="nessus"))
+        scan.action(action="scans/" + str(scan.scan_id) + "/launch",
+                    method="POST")
+        scan.scan_uuid = scan.res["scan_uuid"]
+        print("{i}   SCAN NAME: {n}".format(i=ctinfo, n=scan.scan_name))
+        print("{i}   SCAN UUID: {n}".format(i=ctinfo, n=scan.scan_uuid))
+        self.scanstatus(scan.tag_id, scan.scan_uuid, url, api_akey, api_skey,
+                        insecure)
+        with suppress_stdout():
+            output = str(scan.download_scan(export_format="nessus"))
         return output
+
+    @staticmethod
+    def scanstatus(tag_id, uuid, url, api_akey, api_skey, insecure):
+        running = True
+        counter = 0
+        scan = ness6rest.Scanner(url=url, api_akey=api_akey, api_skey=api_skey,
+                                 insecure=insecure)
+        print("{i}   ".format(i=ctinfo), end='')
+        while running:
+            scan.action(action="scans?folder_id=" + str(tag_id),
+                        method="GET")
+            for s in scan.res["scans"]:
+                if s["uuid"] == uuid and (s['status'] == "running"
+                                          or s['status'] == "pending"):
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+                    time.sleep(2)
+                    counter += 2
+                    if counter % 60 == 0:
+                        print("\n{i}   ".format(i=ctinfo), end='')
+                if s["uuid"] == uuid and s['status'] != "running" and s['status'] != "pending":
+                    running = False
+                    print("\n{i}   Complete! Run time: {s} seconds."
+                          .format(i=ctinfo, s=counter))
 
     @staticmethod
     def writeoutput(xml, results):
