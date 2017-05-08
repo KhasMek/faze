bcfd7155516dd40920c195261c0c1500d6d310d4
KhasMek <Boushh@gmail.com>
2017-05-07 20:04:13 -0600

    misc: add suppress_stdout function
      When we have modules with consolue output we can't
      easily modify the output of to fit in line with faze
      we can use this to just suppress it.
    
      All credit to Dave Smiths post here -
      http://thesmithfam.org/blog/2012/10/25/temporarily-suppress-console-output-in-python/

--- core/helpers/misc.py
+++ core/helpers/misc.py
@@ -3,6 +3,20 @@
 Random functions hang out here until they find a real home
 """
 import os
+import sys
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
 
 
 class ChangeDir:
