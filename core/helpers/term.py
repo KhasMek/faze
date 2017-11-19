3643822dbf3a9a375d7856714ec91854dda472a1
KhasMek <Boushh@gmail.com>
2017-11-19 12:25:22 -0700

    term: add clean_output function.
      This will clean any colors or other unnecessary
      formatting/characters and return a "clean" string.

--- core/helpers/term.py
+++ core/helpers/term.py
@@ -2,6 +2,7 @@
 """
 Consolidated area for terminal formatters.
 """
+import re
 
 from colorama import Fore, Style
 
@@ -9,3 +10,9 @@ from colorama import Fore, Style
 ctact = str(Fore.GREEN + Style.DIM + '[+]' + Style.RESET_ALL)
 cterr = str(Fore.RED + Style.DIM + '[-]' + Style.RESET_ALL)
 ctinfo = str(Fore.BLUE + Style.DIM + '[*]' + Style.RESET_ALL)
+
+
+def clean_output(input):
+    output = re.sub(r'\x1b(\[.*?[@-~]|\].*?(\x07|\x1b\\))', '', input)
+
+    return output
